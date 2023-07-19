73f26797a0d3319179dbf64596ac800b8acf234f
jw418 <ju.wolff@free.fr>
2023-07-19 18:19:32 +0200

    edit const

--- const/constants.ts
+++ const/constants.ts
@@ -3,10 +3,10 @@ import { Mumbai, Chain } from '@thirdweb-dev/chains'
 export const FACTORY_ADDRESS = '0x02101dfB77FDE026414827Fdc604ddAF224F0921'
 export const IMPLEMENTATION_ADDRESS = '0xd3A9848208aD5af494D2B838f8F67949Ac70E3DA'
 
-export const TW_API_KEY = process.env.TW_API_KEY
+export const TW_API_KEY = process.env.TW_API_KEY as string
 
 export const activeChain: Chain = Mumbai
 
-export const NFTDROP_ADDRESS = '0x0Fb1b1C6A1fCf7C9Bf265D3EeCfBfB3b4cC9BfD8'
+export const NFTDROP_ADDRESS = '0xA50dC5793867c903419cf600a9754f5D9A646257'
 export const TOKENDROP_ADDRESS = '0x25007e44be9b80ac23542b1Bf08733cDC057A201'
 export const EDITIONDROP_ADDRESS = '0x7425Ca3E80a3eeA8CbF44D0C76bF2EE759B18E3A'
